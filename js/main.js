08eab1aada05aef3246a63602864f943b116754d
Youssef-zr <yn-neinaa@hotmail.com>
2021-11-20 22:20:29 +0000

    add nav scroll

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ $(() => {
     }, 1200);
 
     // go to top
-
     function checkBtnScroll() {
         if ($(window).scrollTop() > 750) {
             $('.btn-top').parent().fadeIn(500).css({
@@ -41,6 +40,23 @@ $(() => {
 
     $('body').css({
         paddingTop: sectionNav.outerHeight() + 20
+    })
+
+    // navigation item scroll click 
+    $navItem = $('nav .nav-item');
+
+    $navItem.on('click', function (e) {
+        e.preventDefault();
+
+        $link = $(this).find('a');
+        $dateLink = $link.data('link');
+        $scrollTop = $dateLink != "home" ? $('.' + $dateLink).offset().top : 0;
+
+        $link.addClass('active').parent().siblings('li').find('a').removeClass('active');
+        $('html,body').animate({
+            scrollTop: $scrollTop - sectionNav.outerHeight()
+        }, 700)
+
     })
 })
 
